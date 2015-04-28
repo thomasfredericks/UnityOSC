0428f523a241c45b92df50ec4540bfa887e40935
thomasfredericks <thomas.fredericks@gmail.com>
2015-04-27 21:37:53 -0400

    Allow multiple key copies and updated examples

---
+++ Examples/UnityOSC examples - Unity Project/Assets/Scripts/ReceiveFFT.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceiveFFT : MonoBehaviour {
+
+	public OSC oscReference;
+	
+	// Use this for initialization
+	void Start () {
+		oscReference.SetAddressHandler( "/fft" , OnReceive );
+		
+	}
+	
+	// Function that receives the message
+	void OnReceive(OscMessage message) {
+
+		for ( int i = 0 ; i < transform.childCount ; i++) {
+
+			float data = message.GetFloat(i);
+
+			float scaled = Mathf.Lerp(0.1f,4f,data); // Scale to new range
+
+			Transform childTransform = transform.GetChild(i);
+			childTransform.localScale = new Vector3(scaled,0.1f,0.1f);
+		}
+		
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Examples/UnityOSC examples - Unity Project/Assets/Scripts/ReceiveLevel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceiveLevel : MonoBehaviour {
+
+	public OSC oscReference;
+	
+	// Use this for initialization
+	void Start () {
+		oscReference.SetAddressHandler( "/level" , OnReceive );
+	
+	}
+
+	// Function that receives the message
+	void OnReceive(OscMessage message) {
+
+		float data = message.GetFloat(0);
+
+		float scaled = Mathf.Lerp(0.1f,8f,data); // Scale to new range
+
+		transform.localScale = new Vector3(scaled,scaled,scaled);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+	
